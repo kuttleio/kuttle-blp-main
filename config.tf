90071dcc2cc807654ffdbbd8f82cfa19b49a17b3
Dmitriy Solodukha <dspv@me.com>
2023-06-21 22:38:15 -0400

    Add aws provider

--- config.tf
+++ config.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.0"
   backend "s3" {}
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
     logdna = {
       source = "logdna/logdna"
       version = "~> 1.0"
@@ -10,6 +14,14 @@ terraform {
       source = "integrations/github"
       version = "~> 5.0"
     }
+  }
+}
+
+provider aws {
+  region = var.clp_region
+  assume_role {
+    role_arn     = var.provider_role_arn
+    session_name = var.aws_provider_session_name
   }
 }
 
