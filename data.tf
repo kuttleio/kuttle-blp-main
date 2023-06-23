8b942a6310f52a1bbb39f18eb76d97e7ff5d180b
Dmitriy Solodukha <dspv@me.com>
2023-06-23 17:33:56 -0400

    Add zone_id and acm

--- data.tf
+++ data.tf
@@ -1,2 +1,12 @@
 data aws_region current {}
 data aws_elb_service_account main {}
+
+data aws_route53_zone main {
+    name = var.domain_name
+}
+
+data aws_acm_certificate main {
+    domain      = "*.${var.domain_name}"
+    statuses    = ["ISSUED"]
+    most_recent = true
+}
