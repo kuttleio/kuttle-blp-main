30191e6efc94ac05c2e97406a5682865be5209d0
Dmitriy Solodukha <dspv@me.com>
2023-06-24 00:09:59 -0400

    Change maintenance_window

--- dbs.tf
+++ dbs.tf
@@ -11,93 +11,6 @@ locals {
     allocated_storage       = 20
 }
 
-# locals {
-#     engine          = "postgres"
-#     engine_version  = "15.3"
-#     cluster_family  = "postgres15"
-#     cluster_size    = 1
-#     admin_user      = "kuttle"
-#     db_name         = "manifests"
-#     db_port         = 5432
-#     instance_type   = "db.t4g.micro"
-# }
-
-# module postgre {
-#     source  = "cloudposse/rds-cluster/aws"
-#     version = "~> 1.4.0"
-
-#     name            = "${local.name_prefix}-${var.clp_zenv}-PostgreSQL"
-#     engine          = local.engine
-#     cluster_family  = local.cluster_family
-#     cluster_size    = local.cluster_size
-#     admin_user      = local.admin_user
-#     admin_password  = random_password.postgre.result
-#     db_name         = local.db_name
-#     db_port         = local.db_port
-#     instance_type   = local.instance_type
-#     vpc_id          = var.vpc_id
-#     security_groups = var.security_groups
-#     subnets         = var.private_subnets
-#     tags            = var.standard_tags
-# }
-
-# resource random_password postgre {
-#     length  = 24
-#     special = false
-# }
-
-# resource aws_ssm_parameter postgre_connection_string {
-#     name    = "/${local.name_prefix}/${var.clp_zenv}/postgre_connection_string"
-#     type    = "SecureString"
-#     value   = "postgres://${module.postgre.master_username}:${random_password.postgre.result}@${module.postgre.endpoint}/${module.postgre.database_name}"
-#     tags    = var.standard_tags
-# }
-
-# # ---------------------------------------------------
-# #   DB Postgre - Outputs
-# # ---------------------------------------------------
-# output db_name {
-#     value       = module.postgre.database_name
-#     description = "DB name"
-# }
-
-# output db_master_username {
-#     value       = module.postgre.master_username
-#     description = "DB master username"
-#     sensitive   = true
-# }
-
-# output db_cluster_identifier {
-#     value       = module.postgre.cluster_identifier
-#     description = "Cluster Identifier"
-# }
-
-# output db_arn {
-#     value       = module.postgre.arn
-#     description = "RDS Cluster ARN"
-# }
-
-# output db_endpoint {
-#     value       = module.postgre.endpoint
-#     description = "RDS DNS endpoint"
-# }
-
-# output db_reader_endpoint {
-#     value       = module.postgre.reader_endpoint
-#     description = "RDS ReadOnly endpoint"
-# }
-
-# output db_master_host {
-#     value       = module.postgre.master_host
-#     description = "DB Master hostname"
-# }
-
-# output db_replicas_host {
-#     value       = module.postgre.replicas_host
-#     description = "Replicas hostname"
-# }
-
-
 module postgres {
     source  = "terraform-aws-modules/rds/aws"
     version = "~> 5.0"
@@ -118,7 +31,7 @@ module postgres {
     create_db_parameter_group  = false
     subnet_ids                 = var.private_subnets
     vpc_security_group_ids     = var.security_groups
-    maintenance_window         = "Sun:00:00-Sun:03:00"
+    maintenance_window         = "Mon:00:00-Mon:03:00"
     backup_window              = "04:00-06:00"
     backup_retention_period    = 0
     tags                       = var.standard_tags
