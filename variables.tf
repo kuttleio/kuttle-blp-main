5db98eba962c50f02e48711c4a9cd25460570a89
Dmitriy Solodukha <dspv@me.com>
2023-06-23 21:49:58 -0400

    Refactor variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,24 @@
 variable vpc_id {}
-variable clp_region {}
-variable clp_account {}
-variable clp_zenv {}
-variable account_id {}
-variable domain_name {}
-variable ecr_region {}
-variable ecr_account_id {}
-variable standard_tags {}
-variable security_groups {}
-variable mezmo_account_id {}
-
-variable s3_tf_artefacts {}
-
-variable provider_role_arn {}
-variable aws_provider_session_name {}
-
-variable logdna_service_key {}
-variable github_token {}
-variable guthub_owner {}
-
 variable secrets {}
 variable envvars {}
-
+variable clp_zenv {}
+variable clp_region {}
+variable clp_account {}
+variable account_id {}
+variable ecr_region {}
+variable domain_name {}
+variable github_token {}
+variable guthub_owner {}
+variable ecr_account_id {}
+variable standard_tags {}
 variable public_subnets {}
 variable private_subnets {}
+variable security_groups {}
+variable s3_tf_artefacts {}
+variable mezmo_account_id {}
+variable provider_role_arn {}
+variable logdna_service_key {}
+variable aws_provider_session_name {}
 
 variable whitelisted_ips {
   description = "Whitelisted IPs"
