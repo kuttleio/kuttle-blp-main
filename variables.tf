56b3fe51a9b37dee3934ebbe63d5c2965f653e13
Dmitry Varygin <dvarygin78@gmail.com>
2023-06-28 23:39:03 +0300

    Add data for vcpu and memory

--- variables.tf
+++ variables.tf
@@ -15,15 +15,34 @@ variable security_groups {}
 variable s3_tf_artefacts {}
 variable whitelisted_ips {}
 variable mezmo_account_id {}
-variable "database_name" {
+variable database_name {
     default = "environments"
 }
-variable "database_engine_version" {
+variable database_engine_version {
     default = "14"
 }
-variable "database_engine" {
+variable database_engine {
     default = "postgres"
 }
-variable "database_instance_class" {
+variable database_instance_class {
     default = "db.t4g.micro" 
+}
+
+variable frontend_container_cpu {
+    default = "256"
+}
+variable frontend_container_memory {
+    default = "512"
+}
+variable backend_container_cpu {
+    default = "256"
+}
+variable backend_container_memory {
+    default = "512"
+}
+variable runner_container_cpu {
+    default = "256"
+}
+variable runner_container_memory {
+    default = "512"
 }
