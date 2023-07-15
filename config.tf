155cf9629552668cf1e559e9e51350d8360ee074
Dmitriy Solodukha <dspv@me.com>
2023-07-16 00:15:23 +0300

    Fix logdna provider

--- config.tf
+++ config.tf
@@ -5,10 +5,18 @@ terraform {
       source = "integrations/github"
       version = "~> 5.0"
     }
+    logdna = {
+      source = "logdna/logdna"
+      version = "~> 1.0"
+    }
   }
 }
 
 provider github {
   token = data.aws_ssm_parameter.github_token.value
   owner = "kuttleio"
+}
+
+provider logdna {
+  servicekey = data.aws_ssm_parameter.logdna_service_key.value
 }
