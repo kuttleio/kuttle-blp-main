e37a69371af490a7753583421adea47fe7ea3f36
Vincent Liu <vliu@kpler.com>
2023-07-15 22:03:59 +0800

    refactor service, lb, cname, acl assocation

--- cicd.tf
+++ cicd.tf
@@ -1,17 +1,11 @@
-data github_repository frontend {
-  full_name = "kuttleio/frontend"
+data "github_repository" "repositories" {
+  for_each  = var.services
+  full_name = "kuttleio/${each.key}"
 }
 
-data github_repository backend {
-  full_name = "kuttleio/backend"
-}
-
-data github_repository runner {
-  full_name = "kuttleio/runner"
-}
-
-resource github_repository_file frontend {
-  repository          = data.github_repository.frontend.name
+resource "github_repository_file" "respository_files" {
+  for_each            = var.services
+  repository          = data.github_repository.repositories[each.key].name
   branch              = "master"
   file                = ".github/workflows/${local.name_prefix}-${var.clp_zenv}.yaml"
   commit_message      = "Add CICD: delivery from /master to ${var.clp_zenv}"
@@ -20,43 +14,7 @@ resource github_repository_file frontend {
   overwrite_on_create = true
 
   content = templatefile("${path.module}/cicd.tpl.yaml", {
-    service_name  = "frontend"
-    zenv          = title(var.clp_zenv)
-    region        = var.clp_region
-    deploy_branch = "master"
-    cluster_name  = module.ecs_fargate.cluster_name
-  })
-}
-
-resource github_repository_file backend {
-  repository          = data.github_repository.backend.name
-  branch              = "master"
-  file                = ".github/workflows/${local.name_prefix}-${var.clp_zenv}.yaml"
-  commit_message      = "Add CICD: delivery from /master to ${var.clp_zenv}"
-  commit_author       = "kuttle-bot"
-  commit_email        = "kbot@ktl.ai"
-  overwrite_on_create = true
-
-  content = templatefile("${path.module}/cicd.tpl.yaml", {
-    service_name  = "backend"
-    zenv          = title(var.clp_zenv)
-    region        = var.clp_region
-    deploy_branch = "master"
-    cluster_name  = module.ecs_fargate.cluster_name
-  })
-}
-
-resource github_repository_file runner {
-  repository          = data.github_repository.runner.name
-  branch              = "master"
-  file                = ".github/workflows/${local.name_prefix}-${var.clp_zenv}.yaml"
-  commit_message      = "Add CICD: delivery from /master to ${var.clp_zenv}"
-  commit_author       = "kuttle-bot"
-  commit_email        = "kbot@ktl.ai"
-  overwrite_on_create = true
-
-  content = templatefile("${path.module}/cicd.tpl.yaml", {
-    service_name  = "runner"
+    service_name  = each.key
     zenv          = title(var.clp_zenv)
     region        = var.clp_region
     deploy_branch = "master"
--- services.tf
+++ services.tf
@@ -4,18 +4,6 @@
 locals {
   added_env = [
     {
-      name  = "REACT_APP_BACKEND_ENDPOINT"
-      value = "https://${aws_route53_record.backend.fqdn}/api/v1"
-    },
-    {
-      name  = "BACKEND_PATH"
-      value = "https://${aws_route53_record.backend.fqdn}"
-    },
-    {
-      name  = "FRONTEND_PATH"
-      value = "https://${aws_route53_record.frontend.fqdn}"
-    },
-    {
       name  = "QUEUE_URL"
       value = aws_sqs_queue.main.url
     },
@@ -33,7 +21,7 @@ locals {
 # ---------------------------------------------------
 #    Service Discovery Namespace
 # ---------------------------------------------------
-resource aws_service_discovery_private_dns_namespace main {
+resource "aws_service_discovery_private_dns_namespace" "main" {
   name        = "${local.name_prefix}-${var.clp_zenv}-ns"
   description = "${local.name_prefix}-${var.clp_zenv} Private Namespace"
   vpc         = var.vpc_id
@@ -42,85 +30,67 @@ resource aws_service_discovery_private_dns_namespace main {
 # ---------------------------------------------------
 #    Services
 # ---------------------------------------------------
-module frontend {
-  source                  = "github.com/kuttleio/aws_ecs_fargate_app?ref=1.1.1"
-  public                  = true
-  service_name            = "frontend"
-  service_image           = "${aws_ecr_repository.main.repository_url}:frontend"
-  name_prefix             = local.name_prefix
-  standard_tags           = var.standard_tags
-  cluster_name            = module.ecs_fargate.cluster_name
-  zenv                    = var.clp_zenv
-  container_cpu           = var.frontend_container_cpu
-  container_memory        = var.frontend_container_memory
-  vpc_id                  = var.vpc_id
-  security_groups         = var.security_groups
-  subnets                 = var.private_subnets
-  ecr_account_id          = var.account_id
-  ecr_region              = var.ecr_region
-  aws_lb_arn              = aws_lb.frontend.arn
-  aws_lb_certificate_arn  = data.aws_acm_certificate.main.arn
-  logs_destination_arn    = module.logdna.lambda_function_arn
-  domain_name             = var.domain_name
-  task_role_arn           = aws_iam_role.main.arn
-  secrets                 = setunion(var.secrets)
-  environment             = setunion(var.envvars, local.added_env)
+locals {
+  default_common_settings = {
+    public               = false
+    type                 = "non-frontend"
+    name_prefix          = local.name_prefix
+    standard_tags        = var.standard_tags
+    cluster_name         = module.ecs_fargate.cluster_name
+    zenv                 = var.clp_zenv
+    vpc_id               = var.vpc_id
+    security_groups      = var.security_groups
+    subnets              = var.private_subnets
+    ecr_account_id       = var.account_id
+    ecr_region           = var.ecr_region
+    logs_destination_arn = module.logdna.lambda_function_arn
+    domain_name          = var.domain_name
+    task_role_arn        = aws_iam_role.main.arn
+    secrets              = setunion(var.secrets)
+    environment          = setunion(var.envvars, local.added_env)
+  }
+
+  services = {
+    for service_name, service_config in var.services : service_name => merge(
+      local.default_common_settings,
+      {
+        service_name           = service_name,
+        service_image          = "${aws_ecr_repository.main.repository_url}:${service_name}",
+        container_cpu          = service_config.cpu,
+        container_memory       = service_config.memory,
+        aws_lb_arn             = service_config.public ? aws_lb.loadbalancers[service_name].arn : null
+        aws_lb_certificate_arn = service_config.public ? data.aws_acm_certificate.main.arn : null
+        environment            = contains(keys(service_config), "environment") ? concat(local.default_common_settings.environment, service_config.environment) : local.default_common_settings.environment
+      },
+      service_config,
+    )
+  }
 }
 
-module backend {
-  source                  = "github.com/kuttleio/aws_ecs_fargate_app?ref=1.1.1"
-  public                  = true
-  service_name            = "backend"
-  service_image           = "${aws_ecr_repository.main.repository_url}:backend"
-  name_prefix             = local.name_prefix
-  standard_tags           = var.standard_tags
-  cluster_name            = module.ecs_fargate.cluster_name
-  zenv                    = var.clp_zenv
-  container_cpu           = var.backend_container_cpu
-  container_memory        = var.backend_container_memory
-  vpc_id                  = var.vpc_id
-  security_groups         = var.security_groups
-  subnets                 = var.private_subnets
-  ecr_account_id          = var.account_id
-  ecr_region              = var.ecr_region
-  aws_lb_arn              = aws_lb.backend.arn
-  aws_lb_certificate_arn  = data.aws_acm_certificate.main.arn
-  logs_destination_arn    = module.logdna.lambda_function_arn
-  domain_name             = var.domain_name
-  task_role_arn           = aws_iam_role.main.arn
-  secrets                 = setunion(var.secrets)
-  environment             = setunion(var.envvars, local.added_env, [
-    {
-      name  = "UPDATE_STATUSES_CRON"
-      value = "*/10 * * * *"
-    },
-    {
-      name  = "IS_WORKER"
-      value = "1"
-    },
-  ])
+module "services" {
+  for_each               = local.services
+  source                 = "github.com/kuttleio/aws_ecs_fargate_app?ref=1.1.1"
+  public                 = each.value.public
+  service_name           = each.value.service_name
+  service_image          = each.value.service_image
+  name_prefix            = each.value.name_prefix
+  standard_tags          = each.value.standard_tags
+  cluster_name           = each.value.cluster_name
+  zenv                   = each.value.zenv
+  container_cpu          = each.value.container_cpu
+  container_memory       = each.value.container_memory
+  vpc_id                 = each.value.vpc_id
+  security_groups        = each.value.security_groups
+  subnets                = each.value.private_subnets
+  ecr_account_id         = each.value.account_id
+  ecr_region             = each.value.ecr_region
+  aws_lb_arn             = try(each.value.aws_lb_arn, "")
+  aws_lb_certificate_arn = try(each.value.aws_lb_certificate_arn, "")
+  service_discovery_id   = try(each.value.service_discovery_id, "")
+  logs_destination_arn   = each.value.logs_destination_arn
+  domain_name            = each.value.domain_name
+  task_role_arn          = each.value.task_role_arn
+  secrets                = each.value.secrets
+  environment            = each.value.environment
 }
 
-module runner {
-  source                  = "github.com/kuttleio/aws_ecs_fargate_app?ref=1.1.1"
-  public                  = false
-  service_name            = "runner"
-  service_image           = "${aws_ecr_repository.main.repository_url}:runner"
-  name_prefix             = local.name_prefix
-  standard_tags           = var.standard_tags
-  cluster_name            = module.ecs_fargate.cluster_name
-  zenv                    = var.clp_zenv
-  container_cpu           = var.runner_container_cpu
-  container_memory        = var.runner_container_memory
-  vpc_id                  = var.vpc_id
-  security_groups         = var.security_groups
-  subnets                 = var.private_subnets
-  ecr_account_id          = var.account_id
-  ecr_region              = var.ecr_region
-  logs_destination_arn    = module.logdna.lambda_function_arn
-  service_discovery_id    = aws_service_discovery_private_dns_namespace.main.id
-  domain_name             = var.domain_name
-  task_role_arn           = aws_iam_role.main.arn
-  secrets                 = setunion(var.secrets)
-  environment             = setunion(var.envvars, local.added_env)
-}
