0e5486d04b474691c267cea108815a66c7846c7d
Vincent Liu <vliu@kpler.com>
2023-07-15 22:05:46 +0800

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-    region_name_bits    = split("-", var.clp_region)
-    short_region_name   = "${local.region_name_bits[0]}${substr(local.region_name_bits[1], 0, 1)}${substr(local.region_name_bits[2], 0, 1)}"
-    name_prefix         = "${local.short_region_name}-${var.clp_account}"
+  region_name_bits  = split("-", var.clp_region)
+  short_region_name = "${local.region_name_bits[0]}${substr(local.region_name_bits[1], 0, 1)}${substr(local.region_name_bits[2], 0, 1)}"
+  name_prefix       = "${local.short_region_name}-${var.clp_account}"
 }
