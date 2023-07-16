1a2b2d2441620e9f649d2cd377516b0af57b2a23
Dima Solodukha <dspv@me.com>
2023-07-16 14:00:19 +0300

    Remove LogDNA as a Sub Module (#29)

--- modules/logdna/variables.tf
+++
@@ -1,7 +0,0 @@
-variable clp_zenv {}
-variable clp_region {}
-variable clp_account {}
-variable name_prefix {}
-variable standard_tags {}
-variable mezmo_account_id {}
-variable short_region_name {}
