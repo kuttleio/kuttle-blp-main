973d2c068e11f13ab6f9da53aa70abb7393d4a64
Dmitriy Solodukha <dspv@me.com>
2023-07-16 17:04:14 +0300

    Remove LogDNA Errors View

--- logdna.tf
+++ logdna.tf
@@ -5,22 +5,22 @@ resource "logdna_view" "main" {
   tags       = ["${local.name_prefix}-${var.clp_zenv}"]
 }
 
-resource "logdna_view" "errors" {
-  levels     = ["error"]
-  name       = "${var.clp_zenv}-${local.short_region_name} - errors"
-  query      = "-health"
-  categories = ["DEV"]
-  tags       = ["${local.name_prefix}-${var.clp_zenv}"]
+# resource "logdna_view" "errors" {
+#   levels     = ["error"]
+#   name       = "${var.clp_zenv}-${local.short_region_name} - errors"
+#   query      = "-health"
+#   categories = ["DEV"]
+#   tags       = ["${local.name_prefix}-${var.clp_zenv}"]
 
-  # slack_channel {
-  #     immediate       = "true"
-  #     operator        = "presence"
-  #     terminal        = "false"
-  #     triggerinterval = "30"
-  #     triggerlimit    = 1
-  #     url             = var.logdna_slack_non_prod_alerts
-  # }
-}
+#   # slack_channel {
+#   #     immediate       = "true"
+#   #     operator        = "presence"
+#   #     terminal        = "false"
+#   #     triggerinterval = "30"
+#   #     triggerlimit    = 1
+#   #     url             = var.logdna_slack_non_prod_alerts
+#   # }
+# }
 
 # ---------------------------------------------------
 #    LogDNA pushing logs from CloudWatch
