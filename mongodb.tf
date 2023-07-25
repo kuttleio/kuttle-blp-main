3a0969bd03188f29405e95022d0821b4001ae088
Vincent Liu <vliu@kpler.com>
2023-07-25 18:41:29 +0800

    validate

--- mongodb.tf
+++ mongodb.tf
@@ -1,41 +1,16 @@
 locals {
-  region_name_bits  = split("-", var.clp_region)
-  short_region_name = "${local.region_name_bits[0]}${substr(local.region_name_bits[1], 0, 1)}${substr(local.region_name_bits[2], 0, 1)}"
-  short_region_name = "us-west-2"
-  mongodb_region    = "${upper(local.region_name_bits[0])}_${upper(local.region_name_bits[1])}_${upper(local.region_name_bits[2])}" # us-west-2 --> US_WEST_2
-  mongodb_region = "US_WEST_2"
-  name_prefix    = "${local.short_region_name}-${var.clp_account}"
-  standard_tags = merge(var.global_tags, var.env_tags, tomap({
+  mongodb_region = "${upper(local.region_name_bits[0])}_${upper(local.region_name_bits[1])}_${upper(local.region_name_bits[2])}" # us-west-2 --> US_WEST_2
+  standard_tags = merge(var.standard_tags, tomap({
     Name    = "MongoDB Atlas"
     Service = "MongoDB Atlas"
   }))
-  # connection_bits = {
-  #   for k, v in mongodbatlas_cluster.clusters.connection_strings[0].standard_srv : k => v
-  # }
+  connection_bits = {
+    for k, v in mongodbatlas_cluster.clusters : k => v.connection_strings[0].standard_srv
+  }
 }
 
 
 locals {
-  mongodb_atlas = {
-    "dev1" = {
-      "project_name" = "dev1"
-      "cluster1" = {
-        "cluster_type"                = "REPLICASET"
-        "provider_name"               = "TENANT"
-        "backing_provider_name"       = "AWS"
-        "provider_region_name"        = "EU_WEST_1"
-        "provider_instance_size_name" = "M0"
-        "replication_specs"           = {}
-      }
-      "users" = ["admin1", "admin2"]
-    }
-    "dev2" = {
-      "project_name" = "dev2"
-    }
-    "test" = {
-      "project_name" = "test"
-    }
-  }
   default_mongodb_settings = {
     "cluster_type"                 = "REPLICASET"
     "provider_name"                = "TENANT"
@@ -53,7 +28,7 @@ locals {
     }
   }
   mongodb_clusters = { for item in flatten([
-    for project_name, project in local.mongodb_atlas : [
+    for project_name, project in var.mongodb_atlas : [
       for cluster_name, cluster in project : {
         project_name                 = project_name
         cluster_name                 = cluster_name
@@ -70,7 +45,7 @@ locals {
   ]) : "${item.project_name}_${item.cluster_name}" => item }
 
   mongodb_users = { for item in flatten([
-    for project_name, project in local.mongodb_atlas : [
+    for project_name, project in var.mongodb_atlas : [
       for user in project.users : {
         project_name = project_name
         username     = user
@@ -80,15 +55,15 @@ locals {
 }
 
 resource "mongodbatlas_project" "projects" {
-  for_each = local.mongodb_atlas
-  name     = "${local.name_prefix}-${var.clp_wenv}-${each.value.project_name}"
+  for_each = var.mongodb_atlas
+  name     = "${local.name_prefix}-${var.clp_zenv}-${each.value.project_name}"
   org_id   = var.mongodb_atlas_org_id
 }
 
 resource "mongodbatlas_cluster" "clusters" {
   for_each                     = local.mongodb_clusters
   project_id                   = mongodbatlas_project.projects[each.value.project_name].id
-  name                         = "${local.name_prefix}-${var.clp_wenv}-${each.value.project_name}-${each.value.cluster_name}"
+  name                         = "${local.name_prefix}-${var.clp_zenv}-${each.value.project_name}-${each.value.cluster_name}"
   auto_scaling_disk_gb_enabled = each.value.auto_scaling_disk_gb_enabled
   mongo_db_major_version       = each.value.mongo_db_major_version
   cluster_type                 = each.value.cluster_type
@@ -115,16 +90,16 @@ resource "mongodbatlas_cluster" "clusters" {
 # Whitelist NAT IPs
 # ---------------------------------------------------
 resource "mongodbatlas_project_ip_access_list" "vpc_nat_ips" {
-  for_each   = local.mongodb_atlas
+  for_each   = var.mongodb_atlas
   project_id = mongodbatlas_project.projects[each.key].id
-  cidr_block = "${data.terraform_remote_state.vpc.outputs.nat_public_ips[0]}/32"
+  cidr_block = "${var.nat_public_ips[0]}/32"
   comment    = "NAT Gateway IP from AWS VPC"
 }
 
 locals {
   mongodb_project_vpn_ip = merge([
-    for project_name in keys(local.mongodb_atlas) : {
-      for cidr in data.terraform_remote_state.ip_list.outputs.ip_list : "${project_name}|${cidr}" => cidr
+    for project_name in keys(var.mongodb_atlas) : {
+      for cidr in var.vpn_ip_list : "${project_name}|${cidr}" => cidr
     }
   ]...)
 }
@@ -141,10 +116,10 @@ resource "mongodbatlas_project_ip_access_list" "vpn_ip_list" {
 # DATABASE USER  [Configure Database Users](https://docs.atlas.mongodb.com/security-add-mongodb-users/)
 # ---------------------------------------------------
 resource "mongodbatlas_database_user" "users" {
-  for_each   = local.mongodb_users
-  username   = "${local.name_prefix}-${var.clp_wenv}-${each.value.username}"
-  password   = random_password.user_passwords[each.key].result
-  project_id = mongodbatlas_project.projects[each.value.project_name].id
+  for_each           = local.mongodb_users
+  username           = "${local.name_prefix}-${var.clp_zenv}-${each.value.username}"
+  password           = random_password.user_passwords[each.key].result
+  project_id         = mongodbatlas_project.projects[each.value.project_name].id
   auth_database_name = var.auth_database_name
   roles {
     role_name     = "readWriteAnyDatabase"
@@ -153,8 +128,8 @@ resource "mongodbatlas_database_user" "users" {
 
   roles {
     role_name       = "readWrite"
-    database_name   = "${local.name_prefix}-${var.clp_wenv}"
-    collection_name = "${local.name_prefix}-${var.clp_wenv}"
+    database_name   = "${local.name_prefix}-${var.clp_zenv}"
+    collection_name = "${local.name_prefix}-${var.clp_zenv}"
   }
 }
 
