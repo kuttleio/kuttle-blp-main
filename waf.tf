791524a8ad5c42f32b39b761427f092818406db1
Dmitriy Solodukha <dspv@me.com>
2023-07-27 22:05:01 +0300

    Disable WAF for a while

--- waf.tf
+++ waf.tf
@@ -1,86 +1,86 @@
-# ---------------------------------------------------
-#   Web Application Firewall (WAF)
-# ---------------------------------------------------
-resource "aws_wafv2_web_acl" "waf_acl" {
-  name        = "${local.name_prefix}-${var.clp_zenv}-WAF-ACL"
-  description = "WAF ACL"
+# # ---------------------------------------------------
+# #   Web Application Firewall (WAF)
+# # ---------------------------------------------------
+# resource "aws_wafv2_web_acl" "waf_acl" {
+#   name        = "${local.name_prefix}-${var.clp_zenv}-WAF-ACL"
+#   description = "WAF ACL"
 
-  scope = "REGIONAL"
+#   scope = "REGIONAL"
 
-  default_action {
-    allow {}
-  }
+#   default_action {
+#     allow {}
+#   }
 
-  dynamic "rule" {
-    for_each = toset(var.ipwhitelist)
-    content {
-      name     = "whitelisted-ips-rule-${rule.value}"
-      priority = 1
+#   dynamic "rule" {
+#     for_each = toset(var.ipwhitelist)
+#     content {
+#       name     = "whitelisted-ips-rule-${rule.value}"
+#       priority = 1
 
-      action {
-        allow {}
-      }
+#       action {
+#         allow {}
+#       }
 
-      statement {
-        ip_set_reference_statement {
-          arn = aws_wafv2_ip_set.whitelisted_ips[rule.key].arn
-        }
-      }
+#       statement {
+#         ip_set_reference_statement {
+#           arn = aws_wafv2_ip_set.whitelisted_ips[rule.key].arn
+#         }
+#       }
 
-      visibility_config {
-        cloudwatch_metrics_enabled = true
-        metric_name                = "WhitelistedIPsRule"
-        sampled_requests_enabled   = true
-      }
-    }
-  }
+#       visibility_config {
+#         cloudwatch_metrics_enabled = true
+#         metric_name                = "WhitelistedIPsRule"
+#         sampled_requests_enabled   = true
+#       }
+#     }
+#   }
 
-  dynamic "rule" {
-    for_each = toset(var.ipwhitelist)
-    content {
-      name     = "block-non-whitelisted-ips-${rule.value}"
-      priority = 2
+#   dynamic "rule" {
+#     for_each = toset(var.ipwhitelist)
+#     content {
+#       name     = "block-non-whitelisted-ips-${rule.value}"
+#       priority = 2
 
-      action {
-        block {}
-      }
+#       action {
+#         block {}
+#       }
 
-      statement {
-        not_statement {
-          statement {
-            ip_set_reference_statement {
-              arn = aws_wafv2_ip_set.whitelisted_ips[rule.key].arn
-            }
-          }
-        }
-      }
+#       statement {
+#         not_statement {
+#           statement {
+#             ip_set_reference_statement {
+#               arn = aws_wafv2_ip_set.whitelisted_ips[rule.key].arn
+#             }
+#           }
+#         }
+#       }
 
-      visibility_config {
-        cloudwatch_metrics_enabled = true
-        metric_name                = "BlockNonWhitelistedIPsRule"
-        sampled_requests_enabled   = true
-      }
-    }
-  }
+#       visibility_config {
+#         cloudwatch_metrics_enabled = true
+#         metric_name                = "BlockNonWhitelistedIPsRule"
+#         sampled_requests_enabled   = true
+#       }
+#     }
+#   }
 
-  visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name                = "DefaultVisibility"
-    sampled_requests_enabled   = true
-  }
-}
+#   visibility_config {
+#     cloudwatch_metrics_enabled = true
+#     metric_name                = "DefaultVisibility"
+#     sampled_requests_enabled   = true
+#   }
+# }
 
-resource "aws_wafv2_ip_set" "whitelisted_ips" {
-  for_each           = toset(var.ipwhitelist)
-  name               = "${local.name_prefix}-${var.clp_zenv}-Whitelisted-IPs"
-  description        = "Whitelisted IPs"
-  scope              = "REGIONAL"
-  addresses          = [each.key]
-  ip_address_version = "IPV4"
-}
+# resource "aws_wafv2_ip_set" "whitelisted_ips" {
+#   for_each           = toset(var.ipwhitelist)
+#   name               = "${local.name_prefix}-${var.clp_zenv}-Whitelisted-IPs"
+#   description        = "Whitelisted IPs"
+#   scope              = "REGIONAL"
+#   addresses          = [each.key]
+#   ip_address_version = "IPV4"
+# }
 
-resource "aws_wafv2_web_acl_association" "acl_association" {
-  for_each     = { for service_name, service_config in var.services : service_name => service_config if service_config.public == true }
-  resource_arn = aws_lb.loadbalancers[each.key].arn
-  web_acl_arn  = aws_wafv2_web_acl.waf_acl.arn
-}
+# resource "aws_wafv2_web_acl_association" "acl_association" {
+#   for_each     = { for service_name, service_config in var.services : service_name => service_config if service_config.public == true }
+#   resource_arn = aws_lb.loadbalancers[each.key].arn
+#   web_acl_arn  = aws_wafv2_web_acl.waf_acl.arn
+# }
