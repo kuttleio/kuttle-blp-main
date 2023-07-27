d8fa20f8a36775d80795178f7add69c74df9294e
Dima Solodukha <dspv@me.com>
2023-07-27 21:57:51 +0300

    Try to create a new branch first (#74)

--- cicd.tf
+++ cicd.tf
@@ -13,19 +13,19 @@ resource "github_branch" "new" {
   for_each      = local.services
   repository    = data.github_repository.repositories[each.key].name
   branch        = each.value.deploy_branch
-  source_sha    = data.github_branch.master[each.key].commit.0.sha
-  depends_on    = [github_repository_file.respository_files]
+  source_branch = "master"
 }
 
 resource "github_repository_file" "respository_files" {
   for_each            = local.services
   repository          = data.github_repository.repositories[each.key].name
-  branch              = "master" # each.value.deploy_branch
+  branch              = each.value.deploy_branch
   file                = ".github/workflows/${local.name_prefix}-${var.clp_zenv}.yaml"
   commit_message      = "Add CICD: delivery from ${each.value.deploy_branch} to ${var.clp_zenv}"
   commit_author       = "kuttle-bot"
   commit_email        = "kbot@ktl.ai"
   overwrite_on_create = true
+  depends_on          = [github_branch.new]
 
   content = templatefile("${path.module}/cicd.tpl.yaml", {
     service_name   = each.value.name
